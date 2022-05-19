a71dd44deac28fe3fae5d2e9b2dd122c8fe3df01
Robert Cabrera <cabrera.rch@gmail.com>
2022-05-19 10:33:19 +0200

    feat: fix footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,17 +8,21 @@ const Footer = () => {
           <p className="footer__copyright">Robert Cabrera © 2022</p>
         </div>
         <div className="footer__right">
-          <a href="https://twitter.com/RobertCaCh_" className="footer__link" target="_blank" rel="noopener noreferrer">
-            <i className="fa fa-twitter" aria-hidden="true"></i>
+          <a href="mailto:hola@robertcach.com" className="footer__link" target="_blank" rel="noopener noreferrer">
+            <i className="fa fa-envelope" aria-hidden="true"></i>
           </a>
 
           <a href="https://github.com/robertcach" className="footer__link" target="_blank" rel="noopener noreferrer">
             <i className="fa fa-github" aria-hidden="true"></i>
           </a>
 
+          <a href="https://twitter.com/RobertCaCh_" className="footer__link" target="_blank" rel="noopener noreferrer">
+            <i className="fa fa-twitter" aria-hidden="true"></i>
+          </a>
+
           <a href="https://www.linkedin.com/in/robert-cabrera/" className="footer__link" target="_blank" rel="noopener noreferrer">
             <i className="fa fa-linkedin-square" aria-hidden="true"></i>
-          </a>  
+          </a>
         </div>
       </div>
     </footer>
